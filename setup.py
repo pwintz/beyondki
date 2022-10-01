d82be4c120f9e1395fe533b9458a02cb8878d74e
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-10-01 18:50:04 -0500

    Refactor `prerequisites` module and tests.
    This commit bundles the former `is_prerequisites_tag()`,
    `strip_pre_prefix()`, and `extract_prerequisites_tags()` into a single
    `extract_prerequisite_tags()` function. It also adds type hints, lints
    all source files, and generally pushes the repository into a
    PEP-compliant direction.
    
    * Add default py3 `.gitignore`.
    * Rename `tests/main.py` -> `demo.py`.
    * Add `pylintrc`.
    * Add `setup.py`.
    * Normalize package imports.
    * Make `tests/` a python package.
    * Refactor demo logic.
    * Make printing in demo script a bit prettier.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import setuptools
+
+
+setuptools.setup(
+    name="beyondki",
+    version="0.0.1a",
+    description="",
+    url="",
+    author="",
+    author_email="",
+    license="",
+    packages=["beyondki"],
+)
