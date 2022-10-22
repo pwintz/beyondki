ec74858dc1525c047df695efa5932d8c1d4b99cb
Paul Wintz <19618780+pwintz@users.noreply.github.com>
2022-10-22 00:58:57 -0700

    Implement sorting function.

--- card_sorting.py
+++ card_sorting.py
@@ -1,66 +1,33 @@
 """PoC prerequisite sorting."""
-from typing import List
 
-# Define types.
-Cid = int
-CardGraph = dict[Cid, List[Cid]]
+import beyondki.sorting
 
 
 def main() -> None:
     """Run the program."""
 
-    # Create example data.
-    requirement_graph: CardGraph = {1: [2, 3],  # cid=1 with prerequisites 2 and 3
-                                    2: [3],     # cid=2 with prerequisite 3
-                                    3: [],      # cid=3 has no prerequisites
-                                    4: [2]}     # cid=4 with prerequisite 2
-    # TODO: We should be able to generate enablement_graph by inverting requirement_graph.
-    enablement_graph: CardGraph = {1: [],      # cid=1 is not a prerequisites of any cards
-                                   2: [1, 4],  # cid=2 is a prerequisite of 1
-                                   3: [1, 2],  # cid=3 is a prerequisite of 1 and 2
-                                   4: []}      # cid=2 is not a prerequisite of any cards
+    import sys
+    sys.setrecursionlimit(1000)
+    n_cards = 2500 # Works OK up to 1500 with 'dense_prereqs' and up to >1200 with 'sparse_prereqs'.
+    cid = list(range(1, n_cards))
 
-    card_queue: List[Cid] = []
+    def dense_prereqs(cid):
+        # Create a maximally dense requirement_graph by making each card depend on all other
+        # cards that have a lower cid.
+        return list(range(1, cid))
 
-    # TODO: Reorder requirement_graph based on the new card positions of each cid and reorder
-    # TODO: the list values of each entry in enablement_graph by the same criteria.
-    # def new_card_position(cid):
-    #     return cid  # For now, simply use cid. Later, we will need to query the database.
+    def sparse_prereqs(cid):
+        # Create a sparse prerequisite graph.
+        if cid <= 60:
+            return []
+        return [cid-59, cid-60]
 
-    requirement_keys = list(requirement_graph.keys())
+    def reverse_cid_order(cid):
+        return -cid
 
-    def add_and_prune(cid, iteration_index):
-        recursion_index = requirement_keys.index(cid)
-        is_upcoming_cid = recursion_index > iteration_index
+    sorter = beyondki.sorting.CardSorter(cid, sparse_prereqs, reverse_cid_order)
+    card_queue = sorter.sort()
 
-        # If cid still has unmet requirements,
-        # then it is not ready to be added to the card_queue.
-        if len(requirement_graph[cid]) > 0:
-            return
-
-        # If cid is at the current index (as occurs at each iteration step,
-        # at the top of each recursion), or is a previous index
-        # (meaning it was already passed in the iteration but still had unmet requirements),
-        # then add it to the card_queue and set the list of requirements to None instead of an
-        # empty list. Upcoming cards will be added to the card_queue at later step in the iteration.
-        if not is_upcoming_cid:
-            card_queue.append(cid)
-            requirement_graph[cid] = None
-
-            # When cid is added to the card_queue, it can enable a list of other cards,
-            # which are stored in enablement_graph[cid].
-            for enabled_cid in enablement_graph[cid]:
-                # if cid in requirement_graph[enabled_cid]:
-                requirement_graph[enabled_cid].remove(cid)
-                add_and_prune(enabled_cid, iteration_index)
-
-    print(requirement_graph)
-    print(card_queue)
-
-    for index, key in enumerate(requirement_graph.keys()):
-        add_and_prune(key, index)
-
-    print(requirement_graph)
     print(card_queue)
 
 
---
+++ tests/test_CardSorter.py
@@ -0,0 +1,114 @@
+"""Unit tests for ``sorting`` module."""
+from typing import List
+
+import pytest
+from beartype import beartype
+
+from beyondki.sorting import CardSorter
+
+
+def cid_order(cid):
+    return cid
+
+
+def reverse_cid_order(cid):
+    return -cid
+
+
+# noinspection PyUnusedLocal
+def no_prereqs(cid):
+    return []
+
+
+def prereqs_fnc_from_dict(prereq_dict):
+    return lambda cid: prereq_dict[cid]
+
+
+@beartype
+def assert_equal_with_order(dict1: dict[int, List[int]],
+                            dict2: dict[int, List[int]]) -> None:
+    assert dict1 == dict2                            # Check contents
+    assert list(dict1.keys()) == list(dict2.keys())  # Check order
+
+
+def test_single_card():
+    cids = [1]
+    card_sorter = CardSorter(cids, no_prereqs, cid_order)
+    assert card_sorter.cids == [1]
+    assert card_sorter.requirement_graph == {1: []}
+    assert card_sorter.dependents_graph == {1: []}
+
+
+def test_nonexistent_prereq():
+    cids = [1]
+
+    prereqs = prereqs_fnc_from_dict({1: [2]})
+
+    with pytest.raises(ValueError):
+        CardSorter(cids, prereqs, cid_order)
+
+
+def test_sorted_by_cid_wo_prereqs():
+    cids = [2, 1]
+    card_sorter = CardSorter(cids, no_prereqs, cid_order)
+    assert card_sorter.cids == [1, 2]
+    assert_equal_with_order(card_sorter.requirement_graph, {1: [], 2: []})
+    assert_equal_with_order(card_sorter.dependents_graph, {1: [], 2: []})
+
+
+def test_sorted_by_reverse_cid_wo_prereqs():
+    cids = [1, 2]
+    card_sorter = CardSorter(cids, no_prereqs, reverse_cid_order)
+    assert card_sorter.cids == [2, 1]
+    assert_equal_with_order(card_sorter.requirement_graph, {2: [], 1: []})
+    assert_equal_with_order(card_sorter.dependents_graph, {2: [], 1: []})
+
+
+def test_one_prereq():
+    cids = [1, 2]
+
+    def prereqs(cid):
+        if cid == 1:
+            return [2]
+        elif cid == 2:
+            return []
+
+    card_sorter = CardSorter(cids, prereqs, reverse_cid_order)
+    assert_equal_with_order(card_sorter.requirement_graph, {2: [], 1: [2]})
+    assert_equal_with_order(card_sorter.dependents_graph, {2: [1], 1: []})
+
+
+def test_dependent_graph_values_are_sorted():
+    cids = [2, 3, 1]
+
+    def prereqs(cid):
+        if cid == 1:
+            return []
+        else:
+            return [1]
+
+    card_sorter = CardSorter(cids, prereqs, reverse_cid_order)
+    assert_equal_with_order(card_sorter.requirement_graph, {3: [1], 2: [1], 1: []})
+    assert_equal_with_order(card_sorter.dependents_graph, {3: [], 2: [], 1: [3, 2]})
+
+
+def test_error_if_prereqs_not_a_list():
+    # noinspection PyUnusedLocal
+    def return_not_a_list(cid):
+        return 1
+
+    with pytest.raises(ValueError):
+        # noinspection PyTypeChecker
+        CardSorter([1, 2], return_not_a_list, cid_order)
+
+
+# def test_error_if_cids_not_a_list():
+#     cids = 1  # Not a list.
+#
+#     # noinspection PyUnusedLocal
+#     def return_a_list(cid):
+#         return [1]
+#
+#     with pytest.raises(beartype.roar.BeartypeCallHintParamViolation):
+#         # noinspection PyTypeChecker
+#         CardSorter(cids, return_a_list, cid_order)
